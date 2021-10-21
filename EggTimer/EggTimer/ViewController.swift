4f4b5fdf0baf75f76b70b2fe4cf24f12b2574de9
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-10-22 02:48:15 +0530

    eggTimer app course content

---
+++ EggTimer/EggTimer/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Angela Yu on 08/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle
+        switch hardness{
+        case "Soft":
+            print(softTime)
+        case "Medium":
+            print(mediumTime)
+        case "Hard":
+            print(hardTime)
+        default:
+            print("Error")
+        }
+        
+        
+    }
+    
+}
